cb18f48d87baa6ace40d6dc5aaad0fcd181953fb
bobae318 <panut2538@hotmail.com>
2015-03-12 23:31:35 +0700

    Update Main.java
    change  BorderLayout from CENTER to LEFT

--- f2/spw/Main.java
+++ f2/spw/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		GamePanel gp = new GamePanel();
 		GameEngine engine = new GameEngine(gp, v);
 		frame.addKeyListener(engine);
-		frame.getContentPane().add(gp, BorderLayout.CENTER);
+		frame.getContentPane().add(gp, BorderLayout.LEFT);
 		frame.setVisible(true);
 		
 		engine.start();
