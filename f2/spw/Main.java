19a5c54196087e4e76a139604d122be4e74d9f32
bobae318 <Thanaphat.tp@gmail.com>
2015-04-15 23:19:32 +0700

    *03*
    เพิ่มการแสดงผลชื่อ

--- f2/spw/Main.java
+++ f2/spw/Main.java
@@ -3,9 +3,12 @@ package f2.spw;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args){
+		
+		String name = JOptionPane.showInputDialog(null,"Enter name !!","*",JOptionPane.QUESTION_MESSAGE);
 		JFrame frame = new JFrame("Space War");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 650);
@@ -15,9 +18,9 @@ public class Main {
 		GamePanel gp = new GamePanel();
 		GameEngine engine = new GameEngine(gp, v);
 		frame.addKeyListener(engine);
-		frame.getContentPane().add(gp, BorderLayout.LEFT);
+		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
-		
+		JOptionPane.showMessageDialog(null,"Hello  " + name +"  Are you ready to Space Ship","Hi!!" ,JOptionPane.INFORMATION_MESSAGE);
 		engine.start();
 	}
 }
